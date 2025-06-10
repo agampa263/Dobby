2d3028eddeafc270988bc41ff259a1cd356eeb08
agampa263 <akshaykumar_gampa@comcast.com>
2025-06-10 13:17:30 +0000

    test

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -14,9 +14,13 @@ apt-get install -q -y automake libtool autotools-dev software-properties-common 
 pip3 install xmltodict
 pip3 install requests
 ############################
-git clone https://github.com/containers/libocispec.git
+git clone --recurse-submodules https://github.com/containers/libocispec.git
 cd libocispec
 
+git submodule update --init --recursive
+git clean -dfx
+./autogen.sh
+./configure
 make
 make install
 echo "completed building ocispec"
